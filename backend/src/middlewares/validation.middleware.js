44305113a91199ea5e9d83b20c776e5f9959a174
adam <hazimaa1254@gmail.com>
2026-08-05 14:23:48 +0100

    fix: remove unnecessary whitespace in API_URL declaration

--- backend/src/middlewares/validation.middleware.js
+++ backend/src/middlewares/validation.middleware.js
@@ -1,3 +1,4 @@
+
 import { z } from "zod";
 
 const registerSchema = z.object({
--- mobile/services/api.js
+++ mobile/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
-const API_URL = 'http://192.168.1.172:1010/api'; 
+const API_URL ='http://192.168.1.172:1010/api'; 
 
 const api = axios.create({
   baseURL: API_URL,
